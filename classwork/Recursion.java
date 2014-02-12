9a47569433d9ed83631b67c508f7fa20be2c4671
Rushil Kumar <rukumar333@gmail.com>
2014-02-11 21:27:34 -0500

    started work on the hail function

--- classwork/Recursion.java
+++ classwork/Recursion.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Recursion{
 
+    private int count = 0;
+
     public int factorial(int n){
 	int result = n;
 	if(n == 0){
@@ -45,6 +47,26 @@ public class Recursion{
 	}
     }
 
+    public void hail(int x){
+	int n = x;
+	if(x == 1){
+	    System.out.println(x);
+	}else{
+	    if(x % 2 == 0){
+		System.out.println(x);
+		hail(n/2);
+	    }
+	    if(x % 2 == 1){
+		System.out.println(x);
+		hail(3 * n + 1);
+	    }
+	}
+    }
+
+    public int hailln(){
+
+    }
+
     public static void main(String[]args){
 	Recursion r = new Recursion();				    
 	// System.out.println(r.factorial(3));
@@ -52,7 +74,8 @@ public class Recursion{
 	// System.out.println(r.factorial(5));
 	// System.out.println(r.factorial(6));
 	// System.out.println(r.factorial(7));
-	System.out.println(r.fib(45));
-	System.out.println(r.fibltr(45));
+	// System.out.println(r.fib(45));
+	// System.out.println(r.fibltr(45));
+	r.hail(19);
     }
 }
