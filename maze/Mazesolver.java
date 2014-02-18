4f62d3f46cad32f317ac56925e6c414b6fe791b0
Rushil Kumar <rukumar333@gmail.com>
2014-02-17 23:42:24 -0500

    updated mazesolver

--- maze/Mazesolver.java
+++ maze/Mazesolver.java
@@ -75,13 +75,28 @@ public class Mazesolver{
 	    System.out.println("No starting point 'S' found in maze.");
 	}else{
 	    maze[startx][starty]=' ';
-	    solve(maze,startx,starty);
+	    solve(startx,starty);
 	}
     }
 
-    public void solve(char[][]maze,int x,int y){
+    public void solve(int x,int y){
 	wait(20);
 	System.out.println(this);
+
+	if(maze[x][y] == 'X')
+	    solved=true;
+	if(maze[x][y] == ' '){
+	    maze[x][y] = '@';
+	    solve(x+1,y);
+	    if(!solved)
+		solve(x-1,y);
+	    if(!solved)
+		solve(x,y+1);
+	    if(!solved)
+		solve(x,y-1);
+	     if(!solved)
+		 maze[x][y]='.';
+	}
 	
 	//try to solve the maze here...
 
