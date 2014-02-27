292f5a08096c020a40e120a2e3eadc1174bfb123
Rushil Kumar <rukumar333@gmail.com>
2014-02-27 13:16:22 -0500

    started on merge

---
+++ mergesort/MergeSort.java
@@ -0,0 +1,20 @@
+public class MergeSort{
+
+    int[] merge (int[]a;int[]b){
+	while(a.length > 0 || b.length > 0){
+	    if(a.length == 0){
+		int blen = b.length;
+		for(int k = 0; k < blen; k ++){
+		    merge.add(b.remove(0));
+		}
+	    }
+	    if(b.length == 0){
+		int alen = b.length;
+		for(int k = 0; k < alen; k ++){
+		    merge.add(a.remove(0));
+		}
+	    }
+	    if(
+	}
+    }
+}
