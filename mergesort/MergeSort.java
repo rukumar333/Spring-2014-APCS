149b014676d0e1657ed0855bdae2cd5883982258
Rushil Kumar <rukumar333@gmail.com>
2014-03-01 20:24:58 -0500

    finished mergesort

--- mergesort/MergeSort.java
+++ mergesort/MergeSort.java
@@ -1,20 +1,49 @@
+import java.io.*;
+import java.util.*;
+
 public class MergeSort{
 
-    int[] merge (int[]a;int[]b){
-	while(a.length > 0 || b.length > 0){
-	    if(a.length == 0){
-		int blen = b.length;
-		for(int k = 0; k < blen; k ++){
-		    merge.add(b.remove(0));
+    // public int[] merge (int[]a;int[]b){
+    public int[] sort (int[]a,int[]b){
+
+	int track = 0;
+	int btrack = 0;
+	int atrack = 0;
+	int[] result = new int[a.length + b.length];
+	while(atrack < a.length || btrack < b.length){
+	    if(atrack == a.length){
+		for(int k = 0; k + btrack < b.length; k ++){
+		    result[track] = b[k + btrack];
+		    track ++;
+		    btrack ++;
 		}
 	    }
-	    if(b.length == 0){
-		int alen = b.length;
-		for(int k = 0; k < alen; k ++){
-		    merge.add(a.remove(0));
+	    if(btrack == b.length){
+		for(int k = 0; k + atrack < a.length; k ++){
+		    result[track] = a[k + atrack];
+		    track ++;
+		    atrack ++;
 		}
 	    }
-	    if(
+	    if(atrack < a.length && btrack < b.length){
+		if(a[atrack] < b[btrack]){
+		    result[track] = a[atrack];
+		    atrack ++;
+		}else{
+		    result[track] = b[btrack];
+		    btrack ++;
+		}
+		track ++;
+	    }
 	}
+	return result;
+    }
+
+    public static void main(String[] args){
+	int[] a = {1,3,5,10,12};
+	int[] b = {2,8,9,10,100};
+        MergeSort m = new MergeSort();
+	int[] c = m.sort(a,b);
+	System.out.println(Arrays.toString(c));
     }
 }
