c1baf6cb837fa9d6785454762796693a8513e5d7
Rushil Kumar <rukumar333@gmail.com>
2014-03-21 01:15:25 -0400

    need to fix up exception stuff

--- linkedlist/Driver.java
+++ linkedlist/Driver.java
@@ -1,5 +1,11 @@
 public class Driver{
     public static void main(String[] args){
 	MyLinkedList mll = new MyLinkedList();
+	mll.add("Rushil",0);
+	mll.add("Asma",1);
+	System.out.println(mll.get(10));
+	System.out.println(mll.get(1));
+	System.out.println(mll.length());
+	System.out.println(mll.find("Asma"));
     }
 }
--- linkedlist/MyLinkedList.java
+++ linkedlist/MyLinkedList.java
@@ -3,48 +3,86 @@ import java.util.*;
 
 public class MyLinkedList{
 
+    private Node buffer;
     private Node head;
     private Node last;
+    private int length;
 
     public MyLinkedList(){
-	head = null;
-	last = head;
+	buffer = new Node("");
+	head = buffer.getNext();
     }
 
-    public void add(String s){
-	last.setNext(new Node(s));
-	last = last.getNext();
-    }
-
-    public void add(String s, int pos){
-
-    }
-
-    public String get(int pos){
-	Node temp = head;
-	for(int i = 0; i < this.length(); i++){
-	    temp = temp.getNext();
+    public boolean checkInBounds(int pos){
+	if(pos < 0 || pos > length - 1){
+	    return false;
+	}else{
+	    return true;
 	}
     }
 
-    public void set(int pos, String s){
-
+    public Node getNode(int pos) throws Exception{
+	if(!checkInBounds(pos)){
+	    throw new Exception("Position is out of the bounds of the list");
+	}
+	Node temp = buffer;
+	for(int i = 0; i < pos + 1; i ++){
+	    temp = temp.getNext();
+	}
+	return temp;
     }
 
-    public void remove(int pos){
+    public void add(String s, int pos) throws Exception{
+	Node n = new Node(s);
+	Node first = getNode(pos - 1);
+	Node second = first.getNext();
+	n.setNext(second);
+	first.setNext(n);
+	length++;
+    }
 
+    public String get(int pos) throws Exception{
+	if(!checkInBounds(pos)){
+	    throw new Exception("Position is out of the bounds of the list");
+	}
+	return getNode(pos).toString();
+    }
+
+    public void set(String s, int pos) throws Exception{
+	if(!checkInBounds(pos)){
+	    throw new Exception("Position is out of the bounds of the list");
+	}
+	getNode(pos).setData(s);
+    }
+
+    public void remove(int pos) throws Exception{
+	if(!checkInBounds(pos)){
+	    throw new Exception("Position is out of the bounds of the list");
+	}
+	Node one = getNode(pos - 1);
+	Node two = one.getNext();
+	Node three = two.getNext();
+	one.setNext(three);
+	length --;
     }
 
     public int find(String s){
-
+	Node temp = buffer;
+	for(int i = 0; i < length; i ++){
+	    temp = temp.getNext();
+	    if(temp.getData() == s){
+		return i;
+	    }
+	}
+	return -10;
     }
 
     public int length(){
-
+	return length;
     }
 
     public String toString(){
-
+	return "";
     }
 
 }
