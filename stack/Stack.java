bdd06ee34a5715918854538213a2f2cf671213f9
Rushil Kumar <rukumar333@gmail.com>
2014-04-19 12:41:11 -0400

    stack finished

---
+++ stack/Stack.java
@@ -0,0 +1,34 @@
+public class Stack{
+
+    public class Node<T>{
+
+	private T data;
+	private Node<T> next;
+
+
+	public Node(T input){
+	    data = input;
+	    next = null;
+	}
+
+	public Node<T> getNext(){
+	    return next;
+	}
+
+	public T getData(){
+	    return data;
+	}
+
+	public void setData(T input){
+	    data = input;
+	}   
+
+	public void setNext(Node<T> n){
+	    next = n;
+	}
+	public String toString(){
+	    return data.toString();
+	}
+    }
+
+}
